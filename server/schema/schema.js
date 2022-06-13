1b837e050d25d27e616112f763cc6e2a4013ae7a
kriztiank <kriztiank@gmail.com>
2022-06-13 09:17:47 +0200

    Mutation (add)

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -8,7 +8,7 @@ const {
   GraphQLString,
   GraphQLSchema,
   GraphQLList,
-  // GraphQLNonNull,
+  GraphQLNonNull,
   // GraphQLEnumType,
 } = require('graphql')
 
@@ -73,8 +73,32 @@ const RootQuery = new GraphQLObjectType({
   },
 })
 
-//
+// Mutations
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    // Add a client
+    addClient: {
+      type: ClientType,
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) },
+        email: { type: GraphQLNonNull(GraphQLString) },
+        phone: { type: GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parent, args) {
+        const client = new Client({
+          name: args.name,
+          email: args.email,
+          phone: args.phone,
+        })
+
+        return client.save()
+      },
+    },
+  },
+})
 
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation,
 })
