ae40e814ad80410a0276e28dac04138f5694750b
kriztiank <kriztiank@gmail.com>
2022-06-18 10:58:06 +0200

    ADD_CLIENT + DELETE_CLIENT

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,31 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 // import Home from './pages/Home'
 // import Project from './pages/Project'
 // import NotFound from './pages/NotFound'
+import AddClientModal from './components/AddClientModal'
+
+// cache react devtools warning fix
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming
+          },
+        },
+      },
+    },
+  },
+})
 
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql',
-  cache: new InMemoryCache(),
+  cache,
 })
 
 function App() {
@@ -17,6 +38,7 @@ function App() {
       <ApolloProvider client={client}>
         <Header />
         <div className='container'>
+          <AddClientModal />
           <Clients />
         </div>
       </ApolloProvider>
