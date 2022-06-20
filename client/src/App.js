63bd0665b22a220366c6fa577a0d5c54ac110f51
kriztiank <kriztiank@gmail.com>
2022-06-20 10:50:57 +0200

    DELETE_PROJECT

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,9 @@
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 import Header from './components/Header'
-import Clients from './components/Clients'
-import Projects from './components/Projects'
-// import Home from './pages/Home'
-// import Project from './pages/Project'
-// import NotFound from './pages/NotFound'
-import AddClientModal from './components/AddClientModal'
+import Home from './pages/Home'
+import Project from './pages/Project'
+import NotFound from './pages/NotFound'
 
 // cache react devtools warning fix
 const cache = new InMemoryCache({
@@ -37,12 +34,16 @@ function App() {
   return (
     <>
       <ApolloProvider client={client}>
-        <Header />
-        <div className='container'>
-          <AddClientModal />
-          <Projects />
-          <Clients />
-        </div>
+        <Router>
+          <Header />
+          <div className='container'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/projects/:id' element={<Project />} />
+              <Route path='*' element={<NotFound />} />
+            </Routes>
+          </div>
+        </Router>
       </ApolloProvider>
     </>
   )
